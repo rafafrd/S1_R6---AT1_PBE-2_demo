3b64a1874ac75bab927f50d2bc6a48537778fb33
rafafrd <bolekao03@gmail.com>
2026-06-18 08:32:26 -0300

    add tables

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -51,6 +51,25 @@ export async function initializeDatabase() {
     const dbName = process.env.DB_DATABASE || "deploy";
 
     await tempConnection.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\`;`);
+    await tempConnection.query(`USE \`${dbName}\`;`);
+
+    await tempConnection.query(`
+            CREATE TABLE IF NOT EXISTS categorias (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                nome VARCHAR(30) NOT NULL,
+                descricao VARCHAR(300) NULL
+            );
+        `);
+
+    await tempConnection.query(`
+            CREATE TABLE IF NOT EXISTS produtos (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                nome VARCHAR(30) NOT NULL,
+                valor DECIMAL(15,2) NOT NULL,
+                id_categoria INT,
+                FOREIGN KEY (id_categoria) REFERENCES categorias(id)
+            );
+        `);
 
     await tempConnection.end();
     console.log("Banco de dados e tabelas verificados/criados com sucesso.");
